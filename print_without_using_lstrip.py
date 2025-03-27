2845d219b0e5e33c05b143ce97c98d829ce3f64b
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-28 02:17:47 +0800

    slicing to print the rest of the characters

--- print_without_using_lstrip.py
+++ print_without_using_lstrip.py
@@ -9,4 +9,4 @@ for char in range(len(text)):
     if text[char] != " ":          # if yes, break then print the rest of the characters
         break 
 # print output
-print(text[char])
+print(text[char:])
