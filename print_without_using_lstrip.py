533451447ec093786ce049ac411d2a148bf37c55
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-28 02:11:22 +0800

    ask for input

--- print_without_using_lstrip.py
+++ print_without_using_lstrip.py
@@ -1,7 +1,9 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
 # ask the user for input
+text = input("Input: ")
 # inspect each character if it is a non-space character
     # if no, continue 
     # if yes, break then print the rest of the characters
 # print output
+print(text)
