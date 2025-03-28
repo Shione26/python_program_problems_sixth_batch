489f1657a488c87016ae7369ff57bf145478dd38
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 02:07:24 +0800

    used for loop to convert string to uppercase and lowercase and stored in an empty list

--- print_without_using_title.py
+++ print_without_using_title.py
@@ -2,11 +2,19 @@
 
 # ask user for input
 text = input("Input: ")
+
 # create an empty list to store the capitalized texts
 capitalized = []
+
 # split the strings
 splitted_words = text.split()
+
 # convert the first letter to uppercase and the rest to lowercase then store in the list
+for word in splitted_words:
+    first = word[0].upper()
+    rest = word[1:].lower()
+    capitalized.append(first + rest)
+
 # join the splitted words back to one
 # print output
-print(text)
+print(capitalized)
