c7281fe09466cf11d5424263bca952b4fbf90cac
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 01:06:45 +0800

    concatenate the text and the space then print output

--- print_without_using_ljust.py
+++ print_without_using_ljust.py
@@ -1,8 +1,10 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 # ask the user for inputs
-text = input("Input: ")
+text = str(input("Input: "))
 # ask the user for the number of characters included the characters in the left and the space characters
-width = input("Width: ")
+width = int(input("Width: "))
 # concatenate the text and the spaces minus the length of the text
+result = text + " " * (width - len(text))
 # print output
+print(result)
