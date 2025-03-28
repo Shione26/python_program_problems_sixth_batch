9e8a7b2500fbfb79d7f8aaabf75bd708b8ffba9a
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 01:59:58 +0800

    pseudocode

---
+++ print_without_using_title.py
@@ -0,0 +1,8 @@
+# Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+
+# ask user for input
+# create an empty list to store the capitalized texts
+# split the strings
+# convert the first letter to uppercase and the rest to lowercase then store in the list
+# join the splitted words back to one
+# print output
