6f08d71016e15b3e285a94a01a511a476aaf3855
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-28 23:49:09 +0800

    pseudocode

---
+++ print_without_using_isupper.py
@@ -0,0 +1,8 @@
+# Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+
+# ask the user for input
+# define uppercase characters
+# check manually if all characters is on uppercase
+    # if yes, print True
+    # if no, print False
+        
