0158a6b9f336f7a5e4eac51422cbe13648f0e585
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-28 23:16:45 +0800

    created lists for uppercase and lowercase, and initialized empty string

--- print_without_using_lower.py
+++ print_without_using_lower.py
@@ -2,9 +2,16 @@
 
 # ask the user for input
 text = input("Input: ")
+
 # create a list of uppercase letters
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 # create a list of lowercase letters
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+
 # initialize an empty string to store the output
+result = ""
+
 # check if character is in the list of uppercase
     # if yes, find the position of that uppercase 
     # convert it to its corresponding index in lowercase then store in the initialized empty string
