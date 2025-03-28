afdca13baa2aadc362ec1c6a874bcfa5c7a72a1a
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 01:03:05 +0800

    ask for inputs

--- print_without_using_ljust.py
+++ print_without_using_ljust.py
@@ -1,6 +1,8 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
-# ask the user for input 
+# ask the user for inputs
+text = input("Input: ")
 # ask the user for the number of characters included the characters in the left and the space characters
+width = input("Width: ")
 # concatenate the text and the spaces minus the length of the text
 # print output
