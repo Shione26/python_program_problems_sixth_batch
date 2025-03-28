811f419999dcf24eacfd0fd7a64d7be7afc3f43c
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 02:26:05 +0800

    pseudocode

---
+++ print_without_using_swapcase.py
@@ -0,0 +1,9 @@
+# Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+
+# ask user for input
+# initialize an empty string to store the result
+# look through each character in the text
+# check if the character is uppercase
+    # if yes, convert to lowercase and store
+    # else, convert to uppercase and store
+# print output
