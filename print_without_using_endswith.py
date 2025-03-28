6ae650c89aac85f61dfa511a54d584fe57b53633
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 00:30:59 +0800

    ask for inputs

--- print_without_using_endswith.py
+++ print_without_using_endswith.py
@@ -1,7 +1,9 @@
 # Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
 # ask the user for input
+text = input("Input: ")
 # ask the user the suffix
+suffix = input("Suffix: ")
 # check if the suffix is at the end of the index
 # if yes, print True
 # if no, print False
