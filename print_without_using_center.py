079f3149fd88784cab055e7a16aeb8a172d29e9e
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 01:34:47 +0800

    concatenate right and left spaces with the given text then printed output

--- print_without_using_center.py
+++ print_without_using_center.py
@@ -5,6 +5,11 @@ text = input("Input: ")
 # ask the user for width
 width = int(input("Width: "))
 # subtract the length of the text to the width to get the number of spaces on both sides
+total_spaces = width - len(text)
 # divide the number of spaces into 2
+left_spaces = total_spaces // 2
+right_spaces = total_spaces - left_spaces
 # concatenate the spaces with the text
+result = " " * left_spaces + text + " " * right_spaces
 # print output
+print(result)
