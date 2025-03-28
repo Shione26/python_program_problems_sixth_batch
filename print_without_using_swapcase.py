97778fa285c5df768df5504035d46d6d9bc13c3c
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 02:27:03 +0800

    ask for input

--- print_without_using_swapcase.py
+++ print_without_using_swapcase.py
@@ -1,9 +1,12 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 # ask user for input
+text = input("Input: ")
+
 # initialize an empty string to store the result
 # look through each character in the text
 # check if the character is uppercase
     # if yes, convert to lowercase and store
     # else, convert to uppercase and store
 # print output
+print(text)
