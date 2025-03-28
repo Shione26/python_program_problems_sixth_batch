778214c6300cd5bec23258b853f1f419716f44a7
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 00:15:33 +0800

    pseudocode

---
+++ print_without_using_endswith.py
@@ -0,0 +1,7 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# ask the user for input
+# ask the user the suffix
+# check if the suffix is at the end of the index
+# if yes, print True
+# if no, print False
