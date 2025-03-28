aa5eed822b9aecf941b2ccb86d74e4424c13e533
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-28 23:13:38 +0800

    ask for input

---
+++ print_without_using_lower.py
@@ -0,0 +1,12 @@
+# Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# ask the user for input
+text = input("Input: ")
+# create a list of uppercase letters
+# create a list of lowercase letters
+# initialize an empty string to store the output
+# check if character is in the list of uppercase
+    # if yes, find the position of that uppercase 
+    # convert it to its corresponding index in lowercase then store in the initialized empty string
+# print output
+print(text)
