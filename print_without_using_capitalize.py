b2e6a8d76dc024ee2dbbd58a89eae4889dffcad6
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 01:42:51 +0800

    ask for input

---
+++ print_without_using_capitalize.py
@@ -0,0 +1,9 @@
+# Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+
+# ask the user for input
+text = input("Input: ")
+# convert the first string character to uppercase
+# convert the rest string to lowercase
+# concatenate
+# print output
+print(text)
