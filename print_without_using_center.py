60e3b480fef625a69db6282d2d0efe962248a453
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 01:26:07 +0800

    pseudocode

---
+++ print_without_using_center.py
@@ -0,0 +1,8 @@
+# Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# ask the user for input
+# ask the user for width
+# subtract the length of the text to the width to get the number of spaces on both sides
+# divide the number of spaces into 2
+# concatenate the spaces with the text
+# print output
