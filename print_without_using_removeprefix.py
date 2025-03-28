adb3a98961400e9ef6381bb0fc5b6e26c6bfe3f8
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-28 22:47:16 +0800

    ask for inputs

---
+++ print_without_using_removeprefix.py
@@ -0,0 +1,10 @@
+# Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+# ask the user for input
+text = input("Input: ")
+# ask the user for the prefix to be removed
+prefix = input("Prefix: ")
+# check if the text starts with the prefix
+    # if yes, remove the prefix using slicing
+# print the rest of the text
+
