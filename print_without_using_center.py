2c7743ed07ad79959d9baf383fdaa690991e008b
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-29 01:28:08 +0800

    ask for input

--- print_without_using_center.py
+++ print_without_using_center.py
@@ -1,7 +1,9 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 # ask the user for input
+text = input("Input: ")
 # ask the user for width
+width = int(input("Width: "))
 # subtract the length of the text to the width to get the number of spaces on both sides
 # divide the number of spaces into 2
 # concatenate the spaces with the text
